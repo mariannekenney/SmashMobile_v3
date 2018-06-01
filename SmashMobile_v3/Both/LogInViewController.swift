3d33f233df90c2df658eafddfbbbfae17e5c8ff6
Marianne Kenney <mkenney@AD-Mac.local>
2018-06-01 12:21:01 -0400

    First code changes

--- SmashMobile_v3/Both/LogInViewController.swift
+++ SmashMobile_v3/Both/LogInViewController.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2018 SmashFly. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LogInViewController: UIViewController {
+    
+    @IBOutlet weak var email: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        var storyboard = UIStoryboard(name: "HiringManager", bundle: nil)
+        if (email.text?.lowercased() == "recruiter") {
+            storyboard = UIStoryboard(name: "Recruiter", bundle: nil)
+        }
+        let vc = storyboard.instantiateViewController(withIdentifier: "ViewControllerID") as UIViewController
+        present(vc, animated: true, completion: nil)
+    }
+    
+}
