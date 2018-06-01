792a7f4e77f40a261845ddccf4771fc95ae6399b
Marianne Kenney <mkenney@ad-mac.local>
2018-06-01 11:46:07 -0400

    Initial Commit

---
+++ SmashMobile_v3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmashMobile_v3
+//
+//  Created by Marianne Kenney on 6/1/18.
+//  Copyright © 2018 SmashFly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
